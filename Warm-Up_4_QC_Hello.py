dac435dfd8d07622bf8838f3ea09688331f5d3be
yur bee <paul109279@mcpsva.org>
2025-11-21 14:16:17 -0500

    Add files via upload

---
+++ Warm-Up_4_QC_Hello.py
@@ -0,0 +1,19 @@
+"""
+Filename: Warm-Up_4_QC_Hello.py
+Author: <Lastname, Firstname>
+Created: <MM/DD/YYYY>
+Instructor: Holtslander
+"""
+
+def hello():
+    """
+    Asks the user to input their name. Stores their name in a variable and prints the message:
+    "Hello, <name>"
+    :return: None
+    """
+    ### YOUR CODE GOES HERE ###
+
+
+### YOU SHOULD NOT NEED TO CHANGE ANYTHING HERE ###
+if __name__ == '__main__':
+    hello()
